bc783e2b2157e621727d22106eecc9fd4bef3205
E-mailAccses <charlesberry1140@myctl.net>
2026-09-09 19:48:34 +0300

    Delete download.php

--- download.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = 'statement.js';
-header('Content-Type: application/javascript');
-header('Content-Disposition: attachment; filename="' . $file . '"');
-header('Content-Length: ' . filesize($file));
-readfile($file);
-exit;
-?>
